66f98c1fedf04fa8b8e6c66a4d19c7b5e993ae92
Keven Gustavo <kevengustavo2012@gmail.com>
2023-12-04 18:27:34 -0300

    anytree na alocação encadeada

--- Alocação encadeada/mainOS_encadeada.py
+++ Alocação encadeada/mainOS_encadeada.py
@@ -1,17 +1,19 @@
-from alocacao_encadeada import Node,AlocacaoEncadeada
-import os
+from alocacao_encadeada import AlocacaoEncadeada  # Importa a classe de alocação contígua
+import os  # Importa o módulo do sistema operacional
+from anytree import Node, RenderTree  # Importa as classes Node e RenderTree do anytree
 
 class SimpleOSSimulated:
     def __init__(self):
-        self.filesystem = {'root': {'type': 'directory', 'content': {}}}
-        self.current_directory = self.filesystem['root']
+        # Inicialização do sistema operacional simulado
+        self.filesystem = Node("Diretorio-raiz")  # Cria o nó raiz do sistema de arquivos
+        self.current_directory = self.filesystem  # Define o diretório atual como o diretório raiz
 
-        self.file_allocation = AlocacaoEncadeada(disk_space=24)
+        self.file_allocation = AlocacaoEncadeada(disk_space=13)  # Inicializa a alocação contígua com espaço em disco de 13 blocos
 
     def command_prompt(self):
         self.clear_screen()
         while True:
-            command = input("SimpleOS> ").strip().split()
+            command = input("SimpleOS> ").strip().split()  # Recebe o comando do usuário e o separa em partes
             if not command:
                 continue
             if command[0] == "exit":
@@ -27,7 +29,7 @@ class SimpleOSSimulated:
                 if len(command) > 2:
                     self.create_file(command[1], command[2])
                 else:
-                    print("Nome do arquivo, tamanho do arquivo ou tipo de alocacao não especificados.")
+                    print("Nome do arquivo ou tamanho do arquivo não especificados.")
             elif command[0] == "mkdir":
                 if len(command) > 1:
                     self.create_directory(command[1])
@@ -50,6 +52,11 @@ class SimpleOSSimulated:
                     print("Nome do arquivo não especificado")
             elif command[0] == "disk":
                 self.file_allocation.display_disk_allocation()
+            elif command[0] == "getinfo":
+                if len(command) > 1:
+                    self.get_file_info(command[1])
+                else:
+                    print("Nome do arquivo não especificado.")
             elif command[0] == "open":
                 if len(command) > 2:
                     self.open_file(command[1], command[2])
@@ -84,6 +91,7 @@ class SimpleOSSimulated:
         - rename <nome_antigo> <novo_nome>: Renomear um arquivo.
         - remove <nome_arquivo>: Remover um arquivo.
         - disk: Mostrar alocação de disco.
+        - getinfo <nome_arquivo>: Mostrar atributos de arquivo. 
         - open <nome_arquivo> <modo>: Abrir um arquivo.
         - write <conteúdo>: Escrever conteúdo em um arquivo aberto.
         - read <nome_arquivo>: Ler conteúdo de um arquivo.
@@ -93,152 +101,131 @@ class SimpleOSSimulated:
         """)
 
     def clear_screen(self):
+        # Limpa a tela do console, verificando o sistema operacional para usar o comando correto
         os.system('cls' if os.name == 'nt' else 'clear')
 
     def list_directory(self):
+        # Lista o conteúdo do diretório atual e sua árvore hierárquica
         print(f"Diretório atual: C:{self.get_current_directory_path()}")
-        for name, data in self.current_directory['content'].items():
-            if data['type'] == 'directory':
-                print(f"[DIR] {name}")
+        for pre, _, node in RenderTree(self.current_directory):
+            print("%s%s" % (pre, node.name))
+
+    def create_directory(self, directory_name):
+        # Cria um novo diretório com o nome especificado
+        existing_children = [child for child in self.current_directory.children if child.name == directory_name]
+
+        if not existing_children:
+            Node(directory_name, parent=self.current_directory)  # Cria um nó representando o novo diretório
+            print(f"Diretório '{directory_name}' criado com sucesso.")
+        else:
+            print(f"O diretório '{directory_name}' já existe.")
+
+    def change_directory(self, directory_name):
+        # Altera o diretório atual para o especificado
+        if directory_name == "..":
+            # Se o diretório especificado for "..", muda para o diretório pai se não estiver no diretório raiz
+            if self.current_directory != self.filesystem:
+                self.current_directory = self.current_directory.parent
+                print("Retornou ao diretório pai.")
             else:
-                print(f"[FILE] {name}")
-        # Mostrar o diretório atual
-
-    def list_directory(self):
-        print(f"Diretório atual: C:{self.get_current_directory_path()}")
-
-        for name, data in self.current_directory['content'].items():
-            if data['type'] == 'directory':
-                print(f"[DIR] {name}")
+                print("Você já está no diretório raiz.")
+        else:
+            # Verifica se o diretório especificado existe entre os filhos do diretório atual
+            child = next((child for child in self.current_directory.children if child.name == directory_name), None)
+            if child:
+                self.current_directory = child  # Define o diretório atual como o especificado
+                print(f"Entrou no diretório '{directory_name}'.")
             else:
-                print(f"[FILE] {name}")
-        # Mostrar o diretório atual
-        
+                print(f"Diretório '{directory_name}' não encontrado.")
 
-    def get_current_directory_path(self, current_dir=None):
-        # Se o diretório atual não for passado, assume-se como o diretório raiz
-        if current_dir is None:
-            current_dir = self.current_directory
-
-        # Obtém o nome do diretório atual
-        path = [""]
-        for name, content in self.filesystem['root']['content'].items():
-            if content == current_dir:
-                path[0] = name
-                break
-
-        # Se não for o diretório raiz, continua a recursão para os diretórios pai
-        if current_dir != self.filesystem['root']:
-            parent = self.get_parent_directory(current_dir)
-            if parent:
-                path.insert(0, self.get_current_directory_path(parent))
-
-        return '/'.join(path)
-    
+    def get_current_directory_path(self):
+        # Retorna o caminho do diretório atual
+        path = self.current_directory
+        path_str = ''
+        while path.parent:
+            path_str = '/' + path.name + path_str  # Adiciona cada nível do diretório ao caminho
+            path = path.parent
+        return path_str if path_str else '/'  # Retorna o caminho ou '/' se estiver no diretório raiz
+ 
     def create_file(self, filename, file_size):
         try:
             file_size = int(file_size)
         except ValueError:
+            # Verifica se o tamanho do arquivo é um número inteiro positivo
             print("Tamanho do arquivo deve ser um número inteiro positivo.")
             return
 
         if file_size <= 0:
+            # Verifica se o tamanho do arquivo é positivo
             print("Tamanho do arquivo deve ser um número inteiro positivo.")
             return
 
-        if filename in self.current_directory['content']:
+        if any(node.name == filename for node in self.current_directory.children):
+            # Verifica se o arquivo já existe no diretório atual
             print(f"Arquivo '{filename}' já existe.")
         else:
             allocated_blocks = self.file_allocation.allocate_file(filename,file_size)
 
             if allocated_blocks:
-                self.current_directory['content'][filename] = {
-                    'type': 'file',
-                    'size': file_size,
-                    'allocation': allocated_blocks
-                }
+                # Cria um novo nó na árvore para representar o arquivo
+                new_node = Node(filename, parent=self.current_directory, type='file', size=file_size, allocation=allocated_blocks, content='')
                 print(f"Arquivo '{filename}' criado com sucesso.")
             else:
-                print(f"Espaço insuficiente para alocar o arquivo '{filename}' de tamanho {file_size}.")    
-
+                # Indica que não há espaço suficiente para alocar o arquivo
+                print(f"Espaço insuficiente para alocar o arquivo '{filename}' de tamanho {file_size}.")
 
     def remove_file(self, filename):
-        if filename in self.current_directory['content']:
-            file_info = self.current_directory['content'][filename]
-            allocated_block = file_info.get('allocation')
+        # Busca pelo nó do arquivo na pasta atual
+        file_node = next((node for node in self.current_directory.children if node.name == filename and node.type == 'file'), None)
+        
+        if file_node:
+            allocated_block = file_node.allocation
 
             if allocated_block is not None:
+                # Desaloca os blocos alocados para o arquivo
                 success = self.file_allocation.deallocate_blocks(filename)
                 if success:
-                    del self.current_directory['content'][filename]
+                    file_node.parent = None  # Remove o nó do arquivo da árvore
                     print(f"Arquivo '{filename}' removido com sucesso e blocos desalocados.")
                 else:
                     print(f"Falha ao desalocar blocos do arquivo '{filename}'.")
             else:
-                del self.current_directory['content'][filename]
-                print(f"Arquivo '{filename}' removido com sucesso.")    
+                file_node.parent = None  # Remove o nó do arquivo da árvore
+                print(f"Arquivo '{filename}' removido com sucesso.")
         else:
             print(f"Arquivo '{filename}' não encontrado.")
 
-    def create_directory(self, directory_name):
-        if directory_name in self.current_directory['content']:
-            print(f"O diretório '{directory_name}' já existe.")
-        else:
-            self.current_directory['content'][directory_name] = {'type': 'directory', 'content': {}}
-            print(f"Diretório '{directory_name}' criado com sucesso.")
-
-    def change_directory(self, directory_name):
-        if directory_name == "..":
-            # Se o comando for para subir um nível (diretório pai)
-            if self.current_directory != self.filesystem['root']:
-                parent_dir = self.get_parent_directory(self.current_directory)
-                if parent_dir:
-                    self.current_directory = parent_dir
-                    print("Retornou ao diretório pai.")
-                else:
-                    print("Erro ao acessar o diretório pai.")
-            else:
-                print("Você já está no diretório raiz.")
-        elif directory_name in self.current_directory['content'] and self.current_directory['content'][directory_name]['type'] == 'directory':
-            # Se o comando for para entrar em um diretório existente
-            self.current_directory = self.current_directory['content'][directory_name]
-            print(f"Entrou no diretório '{directory_name}'.")
-        else:
-            print(f"Diretório '{directory_name}' não encontrado.")
-
-    def get_parent_directory(self, current_directory):
-        """Obtém o diretório pai do diretório atual."""
-        for content in self.filesystem.values():
-            if 'content' in content and current_directory in content['content'].values():
-                return content
-        return None
-    
     def rename_file(self, old_name, new_name):
-        if old_name in self.current_directory['content']:
-            old_file = self.current_directory['content'][old_name]
+        # Busca pelo nó do arquivo na pasta atual
+        file_node = next((node for node in self.current_directory.children if node.name == old_name and node.type == 'file'), None)
 
-            # Renomeia o arquivo no diretório
-            self.current_directory['content'][new_name] = old_file
-
-            # Remove a entrada do arquivo antigo do diretório
-            del self.current_directory['content'][old_name]
-
-            # Atualiza a entrada de alocação se o arquivo estava alocado
-            if old_name in self.file_allocation.allocated_blocks:
-                allocation_info = self.file_allocation.allocated_blocks.pop(old_name)
-                self.file_allocation.allocated_blocks[new_name] = allocation_info
-
+        if file_node:
+            # Obtém informações sobre a alocação do arquivo pelo nome antigo
+            old_allocation_info = self.file_allocation.allocated_blocks.pop(old_name, None)  # Remove a entrada antiga
+            if old_allocation_info:
+                old_start = old_allocation_info['start']
+                old_size = old_allocation_info['size']
+                new_allocation_info = {'start': old_start, 'size': old_size}
+                self.file_allocation.allocated_blocks[new_name] = new_allocation_info  # Atualiza com o novo nome
+            
+            # Renomeia o nó do arquivo na estrutura da árvore
+            file_node.name = new_name  
             print(f"Arquivo '{old_name}' renomeado para '{new_name}' com sucesso.")
         else:
             print(f"Arquivo '{old_name}' não encontrado.")
 
-    def get_file_info(self, file_name=None):
 
-        if file_name in self.current_directory['content']: # Teste para saber se o arquivo se encontra no diretorio atual
+    def get_file_info(self, file_name=None):
+        # Busca pelo nó do arquivo na pasta atual
+        file_node = next((node for node in self.current_directory.children if node.name == file_name and node.type == 'file'), None)
+
+        if file_node:
+            # Obtém informações básicas sobre o arquivo
             file_path, block_count, start_block = self.get_attributes(file_name)
 
             if file_path is not None and block_count is not None and start_block is not None:
                 if file_path.startswith(f"C:{self.get_current_directory_path()}"):
+                    # Imprime informações sobre o arquivo se estiver no diretório atual
                     print(f"Caminho do arquivo: {file_path}")
                     print(f"Quantidade de blocos: {block_count}")
                     print(f"Primeiro bloco: {start_block}")
@@ -248,32 +235,43 @@ class SimpleOSSimulated:
             print(f"O arquivo '{file_name}' não foi encontrado.")
 
     def get_attributes(self, file_name):
+        # Obtém informações de alocação do arquivo do sistema de arquivos
         allocation_info = self.file_allocation.allocated_blocks.get(file_name)
 
         if allocation_info is not None:
-            file_path = f"C:{self.get_current_directory_path()}/{file_name}"  # Caminho do arquivo
-            block_count = allocation_info['size'] # Tamanho do arquivo
-            start_block = allocation_info['start'] # Onde começa o arquivo no disco
-            return file_path, block_count, start_block
+            # Constrói o caminho do arquivo, tamanho do arquivo e informações de alocação
+            path = f"{self.get_current_directory_path()}/{file_name}"  # Caminho completo do arquivo
+            block_count = allocation_info['size']  # Tamanho do arquivo
+            start_block = allocation_info['start']  # Onde começa o arquivo no disco
+            return path, block_count, start_block
 
         return None, None, None
 
     def open_file(self, file_name, mode):
         try:
-            if file_name in self.current_directory['content'] and self.current_directory['content'][file_name]['type'] == 'file':
-                self.current_file = self.current_directory['content'][file_name]
+            # Verifica se o arquivo está no diretório atual e é um arquivo
+            if any(node.name == file_name and node.type == 'file' for node in self.current_directory.children):
+                self.current_file = file_name  # Define o arquivo atual
                 print(f"Arquivo '{file_name}' aberto no modo '{mode}'.")
             else:
                 print(f"Arquivo '{file_name}' não encontrado ou não é um arquivo.")
-                self.current_file = None  # Se não foi possível abrir, define como None
+                self.current_file = None  # Define como None se não puder abrir
         except KeyError:
             print("Erro ao abrir o arquivo.")
 
+
     def write_to_file(self, content):
         try:
-            if self.current_file and self.current_file['type'] == 'file':
-                self.current_file['content'] = content  # Sobrescreve o conteúdo do arquivo
-                print("Conteúdo escrito com sucesso.")
+            # Verifica se há um arquivo aberto para escrita e se é um arquivo
+            if self.current_file and any(node.name == self.current_file and node.type == 'file' for node in self.current_directory.children):
+                # Atualiza o conteúdo do arquivo
+                for node in self.current_directory.children:
+                    if node.name == self.current_file:
+                        node.content = content
+                        print("Conteúdo escrito com sucesso.")
+                        return
+
+                print("Erro ao encontrar o arquivo para escrita.")
             else:
                 print("Nenhum arquivo aberto para escrita ou o arquivo é inválido.")
         except KeyError:
@@ -281,8 +279,14 @@ class SimpleOSSimulated:
 
     def read_from_file(self, file_name):
         try:
-            if file_name in self.current_directory['content'] and self.current_directory['content'][file_name]['type'] == 'file':
-                content = self.current_directory['content'][file_name]['content']
+            target_node = None
+            for node in self.current_directory.children:
+                if node.name == file_name and node.type == 'file':
+                    target_node = node
+                    break
+            
+            if target_node:
+                content = target_node.content
                 print("Conteúdo do arquivo:")
                 print(content)
             else:
@@ -290,17 +294,19 @@ class SimpleOSSimulated:
         except KeyError:
             print("Erro ao ler o arquivo.")
 
+
     def close_file(self, file_name):
         try:
-            if file_name in self.current_directory['content'] and self.current_directory['content'][file_name]['type'] == 'file':
+            # Verifica se o arquivo está aberto no diretório atual
+            if any(node.name == file_name and node.type == 'file' for node in self.current_directory.children):
                 print(f"Arquivo '{file_name}' fechado com sucesso.")
-                self.current_file = None  # Após fechar, define como None
+                # Define o arquivo atual como None para indicar que nenhum arquivo está aberto
+                self.current_file = None
             else:
-                print(f"Arquivo '{file_name}' não encontrado ou não é um arquivo.")
+                print(f"Arquivo '{file_name}' não encontrado ou não é um arquivo aberto.")
         except KeyError:
             print("Erro ao fechar o arquivo.")
     
-
 # Inicialização do SimpleOS Simulado
 simple_os = SimpleOSSimulated()
 
